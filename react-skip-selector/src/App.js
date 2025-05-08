be7ec4713d234bf67df4099aa8a9128ff518013d
Nabo <anna.muller@pucpr.edu.br>
2025-05-08 16:58:09 -0300

    Commiting the preview of the skip section for the REM Waste.

---
+++ react-skip-selector/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./components/Header";
+import SkipList from "./components/SkipList";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-root">
+      <Header currentStep={2} />
+      <SkipList />
+    </div>
+  );
+}
+
+export default App;
